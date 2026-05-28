551a684e482a2e30fe0c9d72f0c732c16e72b6af
MikhKuts <105879783+MikhKuts@users.noreply.github.com>
2026-05-28 15:45:05 +0300

    Добавлены модели цветов

---
+++ src/main/java/io/github/mikhkuts/Color.java
@@ -0,0 +1,14 @@
+package io.github.mikhkuts;
+
+public class Color {
+    public int red;
+    public int green;
+    public int blue;
+
+
+    public Color(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+}
---
+++ src/main/java/io/github/mikhkuts/StyleColor.java
@@ -0,0 +1,13 @@
+package io.github.mikhkuts;
+
+import java.util.HashMap;
+
+public class StyleColor {
+    public String name;
+    public Color color;
+
+    public StyleColor(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+}
